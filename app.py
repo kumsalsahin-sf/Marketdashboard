a5409e1af8958cfde7bcc8313cf3025e1b43efee
kumsalsahin-sf <ksahin@strategicfit.co.uk>
2025-06-27 13:18:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ elif margin_option == "Negative Margin":
     filtered_df = filtered_df[filtered_df["Margin (€/MWh)"] < 0]
 filtered_df = filtered_df[filtered_df["Segment"].str.contains('|'.join(segment_types))]
 # Sort by increasing price
-filtered_df = filtered_df.sort_values(by="Price (€/MWh)", ascending=True).reset_index(drop=True)
+filtered_df = filtered_df.sort_values(by="Cost (€/MWh)", ascending=True).reset_index(drop=True)
 
 # Chart prep
 volumes = filtered_df["Volume (ktpa)"].to_numpy()
